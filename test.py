3ca85c669e8c96add1721b554704980559dddac1
melek <melekdan@yandex.ru>
2023-01-15 20:18:15 +0300

    add build(dist/main.exe)

--- test.py
+++ test.py
@@ -1,5 +1,1 @@
-from PIL import Image
-for i in range(1, 6):
-    t = Image.open(f"data/level{str(i)}sc.png")
-    t = t.resize((500, 350))
-    t.save(f"data/level{str(i)}sc.png")
+import pyinstaller
