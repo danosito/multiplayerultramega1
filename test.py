52f633431a2f7cfb65598bfdc7f11e1bf6e38c93
melek <melekdan@yandex.ru>
2023-01-15 11:17:03 +0300

    scaled

--- test.py
+++ test.py
@@ -1,5 +1,15 @@
 from PIL import Image
-name = "data/electro_info.png"
-t = Image.open(name)
-t = t.resize((666, 500))
-t.save(name)
+hydro_image = 'hydro.png'
+electro_image = 'electroman.png'
+pyro_image = 'fireman.png'
+cryo_image = 'crioman.png'
+anemo_image = 'anemoman.png'
+geo_image = 'geoman.png'
+dendro_image = 'dendroman.png'
+scaled = []
+perss = [hydro_image, pyro_image, cryo_image, anemo_image, geo_image, electro_image, dendro_image]
+for name in perss:
+    scaled.append("load_image(\"" + name.replace(".png", "") + "_scaled.png\")")
+print("[", end="")
+for i in scaled:
+    print(i, end=", ")
